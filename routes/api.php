6f031d96493cfe3f7830270a33c61fb80031e6fe
Jaynarayan <jkpatel291289@gmail.com>
2023-11-30 19:18:59 +0530

    Finished token auth

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CompleteTaskContoller;
-use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\CompleteTaskContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +18,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
 
-    Route::apiResource('/tasks',TaskController::class);
-    Route::patch('/tasks/{task}/complete',CompleteTaskContoller::class);
+    Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskContoller::class);
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
